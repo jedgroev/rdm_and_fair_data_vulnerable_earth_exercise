bfd5cd520d09932fcdbf8d2939906247d8758302
jedgroev <degroevejohannes@gmail.com>
2026-09-15 19:40:36 +0200

    Fix thesis_data: use same long format as good repo (one row per replicate)

--- thesis_data/analysis_FINAL_v3.R
+++ thesis_data/analysis_FINAL_v3.R
@@ -3,14 +3,13 @@ setwd("C:/Users/alex/Desktop/thesis/data")
 d = read.csv("data_FINAL.csv")
 s = read.csv("sites.csv")
 
-d$mean = (d$m1 + d$m2 + d$m3) / 3
-d2 = d[d$mean != 999, ]
-d2 = d2[!is.na(d2$mean), ]
+d2 = d[d$val != 999, ]
+d2 = d2[!is.na(d2$val), ]
 
 library(lme4)
-m = lmer(mean ~ elev + (1|site), data = d2)
+m = lmer(val ~ elev + (1|site), data = d2)
 summary(m)
 
 library(ggplot2)
-ggplot(d2, aes(x=elev, y=mean)) + geom_point() + geom_smooth(method="lm")
+ggplot(d2, aes(x=elev, y=val)) + geom_point() + geom_smooth(method="lm")
 ggsave("C:/Users/alex/Desktop/thesis/plot1.png")
